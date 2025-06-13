9b066f40da5692b3498726fd1d4f0a3870c3f8b4
Aristide000 <160480222+Aristide000@users.noreply.github.com>
2025-06-13 10:33:32 +0200

    Initial commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [LoginFormComponent],  
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected title = 'angular-forms-demo';
+  title = 'angular-forms-demo';  
 }
